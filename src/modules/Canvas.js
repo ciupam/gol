7c7d11d5553d1c459a8999e729ba6f6107d8dc97
MateuszCiupa <01131212@pw.edu.pl>
2020-05-18 21:54:46 +0200

    canvas update

--- src/modules/Canvas.js
+++ src/modules/Canvas.js
@@ -1,4 +1,9 @@
 export default class Canvas {
+  static DEAD_CELL_FILL = "white";
+  static ALIVE_CELL_FILL = "black";
+  static #HOVER_FILL = "gray";
+  static #LINE_WIDTH = "1";
+
   #canvas;
   #ctx;
 
@@ -10,6 +15,10 @@ export default class Canvas {
     this.#canvas = document.querySelector(queryString);
     this.#ctx = this.#canvas.getContext("2d");
     this.setGrid(gridHeight, gridWidth);
+  }
+
+  get canvas() {
+    return this.#canvas;
   }
 
   getParentWidth() {
@@ -40,9 +49,28 @@ export default class Canvas {
     return [j * this.#cellSize, i * this.#cellSize];
   }
 
+  cordsToCell(x, y) {
+    return [Math.floor(y / this.#cellSize), Math.floor(x / this.#cellSize)];
+  }
+
   drawCell(i, j) {
     this.#ctx.beginPath();
+    this.#ctx.lineWidth = Canvas.#LINE_WIDTH;
     this.#ctx.rect(...this.cellToCords(i, j), this.#cellSize, this.#cellSize);
     this.#ctx.stroke();
+  }
+
+  drawGrid() {
+    for (let i = 0; i < this.#gridHeight; i++)
+      for (let j = 0; j < this.#gridWidth; j++) this.drawCell(i, j);
+  }
+
+  fillCell(i, j, fill = Canvas.ALIVE_CELL_FILL) {
+    this.#ctx.fillStyle = fill;
+    this.#ctx.fillRect(
+      ...this.cellToCords(i, j),
+      this.#cellSize,
+      this.#cellSize
+    );
   }
 }
