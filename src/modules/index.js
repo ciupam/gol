f25a3529d16dc9c2194e262e30425102acdb08f0
MateuszCiupa <01131212@pw.edu.pl>
2020-05-18 20:06:52 +0200

    canvas module

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,6 @@
 import QueryableWorker from "./QueryableWorker";
 import SharedGrid from "./SharedGrid";
 import WorkerManager from "./WorkerManager";
+import Canvas from "./Canvas";
 
-export { QueryableWorker, SharedGrid, WorkerManager };
+export { QueryableWorker, SharedGrid, WorkerManager, Canvas };
